5975ef7ce1d543a8271cfce04253d9bc65dade1e
DivyaShajan <divyadeepthi2913@gmail.com>
2026-06-15 19:12:14 +0200

    bat file

--- opencv_preview-main/opencv_preview-main/parallel_worlds_3_cuda_Timer/build.bat
+++ opencv_preview-main/opencv_preview-main/parallel_worlds_3_cuda_Timer/build.bat
@@ -1,3 +1,15 @@
-# Just double click build.bat
-# OR run from PowerShell:
-Start-Process -FilePath "C:\Users\jabee\Documents\OpenCV-CUDA\opencv_preview-main\opencv_preview-main\parallel_worlds_3_cuda_Timer\build.bat" -Wait
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\18\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+
+cd "C:\Users\jabee\Documents\OpenCV-CUDA\opencv_preview-main\opencv_preview-main\parallel_worlds_3_cuda_Timer"
+
+mkdir build
+cd build
+
+cmake .. -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release
+
+nmake
+
+cd ..
+.\build\parallel_worlds.exe
+pause
